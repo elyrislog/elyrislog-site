d968d2a171ec6082cfc920df60448d7161a606e9
elyrislog <zamboni@idee.tec.br>
2025-12-03 01:31:07 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,13 @@
-
 import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 import Features from '../components/Features'
-import Metrics from '../components/Metrics'
 import Footer from '../components/Footer'
 
 export default function Home(){
   return (
-    <main className="min-h-screen bg-bg-900 text-white">
+    <main className="min-h-screen">
       <Nav />
       <Hero />
-      <Metrics />
       <Features />
       <Footer />
     </main>
