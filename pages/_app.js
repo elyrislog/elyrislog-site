9cf2faff9358db37ecb4d3528170e67718ed95d7
elyrislog <zamboni@idee.tec.br>
2025-12-09 23:28:44 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,18 @@
-import '../styles/globals.css';export default function App({Component,pageProps}){return <Component {...pageProps}/>;}
+import Head from "next/head";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
