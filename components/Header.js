a831d6bbfeaa8ce2004a4cc660c27c820ab4778d
elyrislog <zamboni@idee.tec.br>
2025-12-09 23:38:38 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,7 @@
 
 export default function Header(){
  return(
-  <header style={{padding:'20px 28px',display:'flex',justifyContent:'space-between',alignItems:'center'}}>
-    <strong style={{letterSpacing:1}}>ELYRISLOG</strong>
+  <header style={{padding:'20px 28px',display:'flex',justifyContent:'flex-end',alignItems:'center'}}>
     <nav style={{opacity:.9}}>
       <a href="/">Home</a> · <a href="/about">Sobre</a> · <a href="/services">Serviços</a> · <a href="/contact">Contato</a>
     </nav>
