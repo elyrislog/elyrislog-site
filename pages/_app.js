e1ac96484868977918a00cde89b5f77bd3214274
elyrislog <zamboni@idee.tec.br>
2025-12-12 18:44:55 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,8 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
+<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+
       </Head>
       <Component {...pageProps} />
     </>
